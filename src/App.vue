941c22607ae2ac164951e5ca12b8710b093b29af
jcgsr <cabralguerra@gmail.com>
2020-07-29 10:55:15 -0300

    background 2

--- src/App.vue
+++ src/App.vue
@@ -63,9 +63,10 @@ export default {
 		box-sizing: border-box;
 		color: aliceblue;
 		text-align: center;
-		background: #0f0c29;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #0f0c29, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+		background: #2C3E50;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #4CA1AF, #2C3E50);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #4CA1AF, #2C3E50); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 
 
 	}
